46b7fbab76619bd994726086463c511aa673c4fb
pi-josh <joshua.macatunao007@gmail.com>
2024-04-12 22:07:00 +0800

    Setting up environment and connecting to the database

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const connectDb = require('./config/db');
+
+// connecting to database
+connectDb();
+
+app.get('/AWS', (req, res) => {
+    res.status(200).json({ message: 'Hello AWS'});
+})
+
+app.listen(8080, () => {
+    console.log("Server is running in port 8080");
+})
+
